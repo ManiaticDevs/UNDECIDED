43aeea770cfa652d0ea3dca92806f68ffddb5f7e
Oikmo <78755068+oikmo@users.noreply.github.com>
2021-12-30 19:32:25 +0000

    JOE

--- ASSETS/SCENES/MAIN MENU/MAINMENU.gd
+++ ASSETS/SCENES/MAIN MENU/MAINMENU.gd
@@ -8,7 +8,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/PLAY.grab_focus()
+	$PLAY.grab_focus()
 	$AudioStreamPlayer.play()
 
 func _process(delta):
